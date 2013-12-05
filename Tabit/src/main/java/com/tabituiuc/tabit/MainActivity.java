934e7987f16c4edd5c51cc4a30a36836463e8b55
tabituiuc <zhuang45@illinois.edu>
2013-12-05 22:46:56 +0800

    Final Submission 20131205

--- Tabit/src/main/java/com/tabituiuc/tabit/MainActivity.java
+++ Tabit/src/main/java/com/tabituiuc/tabit/MainActivity.java
@@ -270,6 +270,7 @@ public class MainActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
          switch (item.getItemId()) {
+
             case R.id.action_settings:
                 return true;
         }
